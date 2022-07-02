bdf1f44d6a17faf506210c18e95d8a0a53b353b5
Emir Yarkın Yaman <65957512+WEINOOSE@users.noreply.github.com>
2022-07-02 21:36:06 +0300

    Update DB2022.py

--- src/DB2022.py
+++ src/DB2022.py
@@ -1,10 +1,11 @@
 DRS = 500
+from random import randint
 
 import pandas as pd
 last_df_df = pd.DataFrame()
 
 class Track():
-    def __init__(self,title,country,performance,speed,wing,chassis,deg,rain_odd,time,lap,sc):
+    def __init__(self,title,country,performance,speed,wing,chassis,deg,rain_odd,time,lap,sc,overtake_chance):
         self.title = title
         self.country = country
         self.performance = performance
@@ -16,6 +17,7 @@ class Track():
         self.time = time
         self.lap = lap
         self.sc = sc
+        self.overtake_chance = overtake_chance
 
 # for 2022 regulations, here is the plus' and minus' values.
 # Top Speed = -1, Acceleration = 1
@@ -25,41 +27,41 @@ class Track():
 
 # Title,Name,Performance,Speed,Wing,Chassis,Degration between 0.90 and 1.10, Time, Lap
 
-bhr = Track('Bahrain GP','Bahrain',-1,-1,1,-1,1.10,1.5,90.5,57,15)
-skh = Track('Sakhir GP','Sakhir',-1,-1,-1,-1,1.10,1.5,55.5,87,10)
-ksa = Track('Saudi Arabian GP','Jeddah',-1,1,1,1,0.90,1.5,88.2,50,65)
-aus = Track('Australian GP','Melbourne',1,1,1,1,0.90,12.5,78,58,50)
-mal = Track('Malaysian GP','Sepang',-1,-1,-1,-1,1.10,25.5,92.5,56,15)
-chn = Track('Chinese GP','Shangai',-1,1,1,-1,1.10,35.5,94,56,25)
-can = Track('Canadian GP','Montreal',1,1,1,0,1.05,2.5,72.5,70,60)
-mia = Track('Miami GP','Miami',1,1,1,1,1.05,3.5,88.5,45,65)
-mnk = Track('Monaco GP','Monaco',1,1,1,-1,0.95,2.5,72.5,78,70)
-esp = Track('Spanish GP','Catalunya',-1,1,1,1,1.10,1.5,78.5,66,25)
-eif = Track('Eifel GP','Nordschleife',1,1,1,1,1.05,25.5,87,60,25)
-imo = Track('Emilia Romagna GP','Imola',-1,-1,-1,1,1.00,42.5,75.5,63,25)
-mug = Track('Tuscan GP','Mugello',-1,1,1,1,0.90,12.5,77,59,25)
-rus = Track('Russian GP','Sochi',1,-1,1,-1,0.90,40.5,94,53,50)
-aze = Track('Azerbaijan GP','Baku',-1,1,1,-1,0.90,2.5,103,51,60)
-tur = Track('Turkish GP','Istanbul',-1,-1,1,1,0.90,35.5,84,58,25)
-eur = Track('European GP','Valencia',1,1,1,-1,0.90,1.5,96,57,65)
-por = Track('Portuguese GP','Portimao',-1,1,-1,1,1.00,5.5,78.5,66,25)
-gbr = Track('British GP','Silverstone',-1,1,-1,-1,0.95,37.5,87.5,52,25)
-ger = Track('German GP','Höckenheim',-1,1,1,1,1.05,37.5,74.5,67,25)
-dut = Track('Dutch GP','Zandvoort',-1,1,1,1,1.05,17.5,70,72,25)
-aut = Track('Austrian GP','Spielberg',-1,-1,-1,1,0.95,17.5,65.5,71,25)
-fra = Track('French GP','Le Castellet',-1,1,1,1,1.15,17.5,90.5,53,25)
-hun = Track('Hungarian GP','Hungary',1,1,1,1,1.05,22.5,77,70,25)
-bel = Track('Belgian GP','Spa-Francorchamps',-1,-1,-1,1,1.00,37.5,104.5,44,25)
-ita = Track('Italian GP','Monza',-1,-1,-1,1,0.90,12.5,81.5,53,25)
-sin = Track('Singapore GP','Marina Bay',1,1,1,-1,1.05,25.5,99,59,70)
-fuj = Track('Pacific GP','Fuji',1,1,-1,1,1.10,35.5,82.5,67,25)
-jpn = Track('Japanese GP','Suzuka',-1,1,-1,1,1.15,10.5,90,53,25)
-usa = Track('United States GP','Austin',-1,1,1,1,0.90,12.5,94.5,56,25)
-mex = Track('Mexico City GP','Mexico City',-1,-1,-1,-1,1.00,1.5,76.5,71,25)
-bra = Track('Brazilian GP','Interlagos',-1,-1,-1,1,1.05,42.5,70,71,45)
-ind = Track('Indian GP','India',-1,1,1,-1,1.10,25.5,82,60,25)
-qat = Track('Qatar GP','Qatar',-1,1,-1,1,0.90,1.5,82,57,25)
-abu = Track('Abu Dhabi GP','Abu Dhabi',-1,1,1,-1,0.90,1.5,97,55,45)
+bhr = Track('Bahrain GP','Bahrain',-1,-1,1,-1,1.10,1.5,90.5,57,15,0.86)
+skh = Track('Sakhir GP','Sakhir',-1,-1,-1,-1,1.10,1.5,55.5,87,10,0.90)
+ksa = Track('Saudi Arabian GP','Jeddah',-1,1,1,1,0.90,1.5,88.2,50,65,0.65)
+aus = Track('Australian GP','Melbourne',1,1,1,1,0.90,12.5,78,58,50,0.65)
+mal = Track('Malaysian GP','Sepang',-1,-1,-1,-1,1.10,25.5,92.5,56,15,0.80)
+chn = Track('Chinese GP','Shangai',-1,1,1,-1,1.10,35.5,94,56,25,0.70)
+can = Track('Canadian GP','Montreal',1,1,1,0,1.05,2.5,72.5,70,60,0.65)
+mia = Track('Miami GP','Miami',1,1,1,1,1.05,3.5,88.5,45,65,0.65)
+mnk = Track('Monaco GP','Monaco',1,1,1,-1,0.95,2.5,72.5,78,70,0.76)
+esp = Track('Spanish GP','Catalunya',-1,1,1,1,1.10,1.5,78.5,66,25,0.30)
+eif = Track('Eifel GP','Nordschleife',1,1,1,1,1.05,25.5,87,60,25,0.60)
+imo = Track('Emilia Romagna GP','Imola',-1,-1,-1,1,1.00,42.5,75.5,63,25,0.45)
+mug = Track('Tuscan GP','Mugello',-1,1,1,1,0.90,12.5,77,59,25,0.60)
+rus = Track('Russian GP','Sochi',1,-1,1,-1,0.90,40.5,94,53,50,0.45)
+aze = Track('Azerbaijan GP','Baku',-1,1,1,-1,0.90,2.5,103,51,60,0.70)
+tur = Track('Turkish GP','Istanbul',-1,-1,1,1,0.90,35.5,84,58,25,0.70)
+eur = Track('European GP','Valencia',1,1,1,-1,0.90,1.5,96,57,65,0.60)
+por = Track('Portuguese GP','Portimao',-1,1,-1,1,1.00,5.5,78.5,66,25,0.85)
+gbr = Track('British GP','Silverstone',-1,1,-1,-1,0.95,37.5,87.5,52,25,0.85)
+ger = Track('German GP','Höckenheim',-1,1,1,1,1.05,37.5,74.5,67,25,0.70)
+dut = Track('Dutch GP','Zandvoort',-1,1,1,1,1.05,17.5,70,72,25,0.70)
+aut = Track('Austrian GP','Spielberg',-1,-1,-1,1,0.95,17.5,65.5,71,25,0.90)
+fra = Track('French GP','Le Castellet',-1,1,1,1,1.15,17.5,90.5,53,25,0.50)
+hun = Track('Hungarian GP','Hungary',1,1,1,1,1.05,22.5,77,70,25,0.50)
+bel = Track('Belgian GP','Spa-Francorchamps',-1,-1,-1,1,1.00,37.5,104.5,44,25,0.90)
+ita = Track('Italian GP','Monza',-1,-1,-1,1,0.90,12.5,81.5,53,25,0.90)
+sin = Track('Singapore GP','Marina Bay',1,1,1,-1,1.05,25.5,99,59,70,0.45)
+fuj = Track('Pacific GP','Fuji',1,1,-1,1,1.10,35.5,82.5,67,25,0.70)
+jpn = Track('Japanese GP','Suzuka',-1,1,-1,1,1.15,10.5,90,53,25,0.60)
+usa = Track('United States GP','Austin',-1,1,1,1,0.90,12.5,94.5,56,25,0.65)
+mex = Track('Mexico City GP','Mexico City',-1,-1,-1,-1,1.00,1.5,76.5,71,25,0.65)
+bra = Track('Brazilian GP','Interlagos',-1,-1,-1,1,1.05,42.5,70,71,45,0.85)
+ind = Track('Indian GP','India',-1,1,1,-1,1.10,25.5,82,60,25,0.65)
+qat = Track('Qatar GP','Qatar',-1,1,-1,1,0.90,1.5,82,57,25,0.75)
+abu = Track('Abu Dhabi GP','Abu Dhabi',-1,1,1,-1,0.90,1.5,97,55,45,0.65)
 
 circuits = [bhr,skh,ksa,aus,mal,chn,can,mia,mnk,eif,imo,mug,rus,aze,tur,esp,eur,por,gbr,ger,dut,
 aut,fra,hun,bel,ita,sin,fuj,jpn,usa,mex,bra,ind,qat,abu]
@@ -98,9 +100,8 @@ i = Tyre('Intermediate',10.350,0.750)
 w = Tyre('Wet',25.350,0.650)
 
 compounds = [s,h,i,w]
-collision = 8
-stamina = 4
-safety = 40
+upper_stamina = 15
+lower_stamina = 5
 # Constants
 weather = 0
 start = 1
